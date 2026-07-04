803257ca9cf12e460da9e27bdfc26b2e0426091c
Miguel-AGC <magcampuzano.1410@gmail.com>
2026-07-04 17:57:29 -0600

    Añadir clase abstracta BaseEntity con propiedades comunes
    Se crea la clase BaseEntity en POSBicipartes.Domain.Common, incluyendo propiedades para Id, fechas de creación y actualización, estado de actividad y métodos para gestionar estos valores. Facilita la reutilización y consistencia en entidades del dominio.

---
+++ POSBicipartes.Domain/Common/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿namespace POSBicipartes.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; protected set; }
+
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; private set; }
+
+    public bool IsActive { get; private set; } = true;
+
+    public void UpdateTimestamp()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void Activate()
+    {
+        IsActive = true;
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
+    }
+}
