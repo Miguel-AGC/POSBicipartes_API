057dba292643b9015a34e4d6e5781d0e25a7a347
Miguel-AGC <magcampuzano.1410@gmail.com>
2026-07-04 18:25:54 -0600

    Configuración inicial de EF Core para SystemConfiguration
    Se agregan ApplicationDbContext y la configuración de la entidad SystemConfiguration, definiendo restricciones, valores por defecto y precisiones. Se registra el DbSet y se aplican automáticamente las configuraciones desde el ensamblado.

---
+++ POSBicipartes.Infraestructure/Persistence/Configurations/SystemConfigurationConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POSBicipartes.Domain.Entities;
+
+namespace POSBicipartes.Infrastructure.Persistence.Configurations;
+
+public class SystemConfigurationConfiguration : IEntityTypeConfiguration<SystemConfiguration>
+{
+    public void Configure(EntityTypeBuilder<SystemConfiguration> builder)
+    {
+        builder.ToTable("system_configuration");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.BusinessName)
+            .HasMaxLength(150)
+            .IsRequired();
+
+        builder.Property(x => x.TradeName)
+            .HasMaxLength(150);
+
+        builder.Property(x => x.OwnerName)
+            .HasMaxLength(150);
+
+        builder.Property(x => x.Address)
+            .HasMaxLength(300);
+
+        builder.Property(x => x.Phone)
+            .HasMaxLength(20);
+
+        builder.Property(x => x.Email)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.TaxId)
+            .HasMaxLength(30);
+
+        builder.Property(x => x.LogoPath)
+            .HasMaxLength(300);
+
+        builder.Property(x => x.Currency)
+            .HasMaxLength(5)
+            .HasDefaultValue("MXN");
+
+        builder.Property(x => x.TaxPercentage)
+            .HasPrecision(5, 2);
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+
+        builder.Property(x => x.IsActive)
+            .HasDefaultValue(true);
+    }
+}
---
+++ POSBicipartes.Infraestructure/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POSBicipartes.Domain.Entities;
+
+namespace POSBicipartes.Infrastructure.Persistence.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<SystemConfiguration> SystemConfigurations => Set<SystemConfiguration>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
